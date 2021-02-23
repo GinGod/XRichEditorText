3802048160beb0f2538c792f390f26c5a1451af8
马金刚 <decetwen1989@163.com>
2021-02-23 16:49:45 +0800

    implementation 'com.github.sendtion:XRichText:1.9.4'

---
+++ xricheditortextlib/src/main/java/com/gingod/xricheditortextlib/IImageLoader.java
@@ -0,0 +1,7 @@
+package com.gingod.xricheditortextlib;
+
+import android.widget.ImageView;
+
+public interface IImageLoader {
+    void loadImage(String imagePath, ImageView imageView, int imageHeight);
+}
